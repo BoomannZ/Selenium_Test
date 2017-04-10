c29bf4318e56c8b9d59ae067a542223ad7710e7e
kpapian <kpapyan@mail.ua>
2017-04-10 22:56:20 +0300

    Add 2 tests - Create and Delete repository option

---
+++ SeleniumFirst/SeleniumFirst/CreateRepository.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Threading;
+
+namespace SeleniumFirst
+{
+      public class CreateRepository
+    {
+
+
+        private IWebDriver _driver;
+
+        public void CreateDriver()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("start-maximized");
+
+            _driver = new ChromeDriver(options);
+
+        }
+
+
+        public void LogInOnGitHub()
+        {
+            _driver.Url = "https://github.com/";
+
+            IWebElement elSignIn = _driver.FindElement(By.LinkText("Sign in"));
+            elSignIn.Click();
+
+            IWebElement elLogin = _driver.FindElement(By.Id("login_field"));
+            elLogin.SendKeys("kpapian");
+
+            IWebElement elPassword = _driver.FindElement(By.Id("password"));
+            elPassword.SendKeys("14151820a" + Keys.Enter);
+
+        }
+
+
+        public void CreateRepo()
+        {
+            _driver.FindElement(By.XPath(".//*[@id='user-links']//li[2]")).Click();
+
+            IWebElement createNewRepo = _driver.FindElement(By.XPath(".//*[@id='user-links']//ul/a[1]"));
+            Thread.Sleep(3000);
+
+            createNewRepo.Click();
+
+            IWebElement repoName = _driver.FindElement(By.Id("repository_name"));
+            repoName.SendKeys("Test_repo");
+
+            Thread.Sleep(2000);
+
+            _driver.FindElement(By.XPath(".//*[@id='new_repository']/div[4]/button")).Submit();
+
+            By elLocDropDown = By.XPath(".//*[@class='avatar']");
+            IWebElement elementDropDown = _driver.FindElement(elLocDropDown);
+            elementDropDown.Click();
+
+            By elLocProfile = By.XPath(".//*[@id='user-links']//div/a[1]");
+            IWebElement elementProfile = _driver.FindElement(elLocProfile);
+            elementProfile.Click();
+
+            By repositorys = By.XPath(".//div[3]/nav/a[2]");
+            IWebElement repoTab = _driver.FindElement(repositorys);
+            repoTab.Click();
+
+            By newRepos = By.LinkText("Test_repo");
+            IWebElement rep = _driver.FindElement(newRepos);
+
+            Assert.AreEqual("Test_repo", rep.Text);
+
+        }
+    }
+}
---
+++ SeleniumFirst/SeleniumFirst/DeleteRepository.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+using System.Threading;
+
+namespace SeleniumFirst
+{
+    public class DeleteRepository
+    {
+                       
+            private IWebDriver _driver;
+
+            public void CreateDriver()
+            {
+                ChromeOptions options = new ChromeOptions();
+                options.AddArguments("start-maximized");
+
+                _driver = new ChromeDriver(options);
+
+            }
+
+
+            public void LogInOnGitHub()
+            {
+                _driver.Url = "https://github.com/";
+
+                IWebElement elSignIn = _driver.FindElement(By.LinkText("Sign in"));
+                elSignIn.Click();
+
+                IWebElement elLogin = _driver.FindElement(By.Id("login_field"));
+                elLogin.SendKeys("kpapian");
+
+                IWebElement elPassword = _driver.FindElement(By.Id("password"));
+                elPassword.SendKeys("14151820a" + Keys.Enter);
+
+            }
+
+            public void DeleteRepo()
+            {
+
+                By elLocDropDown = By.XPath(".//*[@class='avatar']");
+                IWebElement elementDropDown = _driver.FindElement(elLocDropDown);
+                elementDropDown.Click();
+
+                By elLocProfile = By.XPath(".//*[@id='user-links']//div/a[1]");
+                IWebElement elementProfile = _driver.FindElement(elLocProfile);
+                elementProfile.Click();
+
+                By repositorys = By.XPath(".//div[3]/nav/a[2]");
+                IWebElement repoTab = _driver.FindElement(repositorys);
+                repoTab.Click();
+
+                By newRepos = By.LinkText("Test_repo");
+                IWebElement rep = _driver.FindElement(newRepos);
+                rep.Click();
+
+                By settings = By.LinkText("Settings");
+                IWebElement settingsTab = _driver.FindElement(settings);
+                settingsTab.Click();
+
+                By deleteBtn = By.XPath(".//*[@id='options_bucket']//div/button[3]");
+                IWebElement deleteButon = _driver.FindElement(deleteBtn);
+                deleteButon.Click();
+            
+                Assert.AreEqual("Test_repo", rep.Text);
+
+            }
+        }
+}
